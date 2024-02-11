dc7451a449f505e18ced28e8f489162218563af9
Nick Pirocanac <nick@zaphop.com>
2024-02-11 14:37:25 -0600

    Changing to only support x64 binaries.

--- Program.cs
+++ Program.cs
@@ -14,11 +14,27 @@ namespace com.zaphop.nvidiabroadcast
     {
         static void Main(string[] args)
         {
+            //TestBroadcastManager();
+
             // Uncomment this line of code to allow for debugging
             // This will cause the application to wait until you attach to the running process from Visual Studio.
             //while (!System.Diagnostics.Debugger.IsAttached) { System.Threading.Thread.Sleep(100); }
 
             SDWrapper.Run(args);
+        }
+
+        private static void TestBroadcastManager()
+        {
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("pt-BR");
+            System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+
+            Windows.Win32.PInvoke.SetThreadLocale(1046);
+            Windows.Win32.PInvoke.SetThreadUILanguage(1046); // This one did the trick!
+
+            var x = Windows.Win32.PInvoke.GetThreadLocale();
+
+            NvidiaBroadcastManager n = new NvidiaBroadcastManager(NvidiaBroadcastResourceID.AutoFrame, "FaceZoom");
         }
     }
 }
